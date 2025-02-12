e88cfc9617b256cb35bfe094126607aa18ca3cd1
chonchol <chonchol57@gmail.com>
2025-02-12 12:38:21 +0600

    added the design based on parameter passed

--- src/App.jsx
+++ src/App.jsx
@@ -5,37 +5,89 @@ const App = () => {
   const notificationSettings = {
     notify_text: "Custom code is not validated.",
     notify_desc: [
-      { text: "Please check the code and try again." },
-      { text: "If you think this is a mistake, please contact support." },
+      {
+        desc: true,
+        text: "Incorrect code may impact your website's performance",
+      },
     ],
-    notify_btn: true,
-    button_text: "Ok, I got it",
-    redirect_url: "#",
-    notify_icon: true,
-    notify_close: true,
+    notify_btn: { btn: true, text: "Ok, I got it!" },
+    redirect_url: { redirect: false, url: "#", text: "Maybe later" },
+    notify_icon: { icon: false, color: "#E2E1E7" },
+    notify_close: { close_n: false, color: "#E2E1E7" },
     notify_color: {
-      background: "#ff0000",
-      text: "#ffffff",
-      icon: "#ffffff",
+      background: "#F7F7F9",
+      text: "#DAD9DE",
       border: {
         type: true,
-        color: "5px solid #f1c40f;",
+        color: "5px solid #E2E1E7",
       },
     },
     progress_bar: {
-      type: true,
+      type: false,
       background: "#ff0000",
     },
   };
 
+  const notificationSettings2 = {
+    notify_text: "The data export you requested is ready!",
+    notify_desc: [
+      {
+        desc: false,
+        text: "Incorrect code may impact your website's performance",
+      },
+    ],
+    notify_btn: { btn: true, text: "View the data" },
+    redirect_url: { redirect: false, url: "#", text: "View the data" },
+    notify_icon: { icon: true, color: "#4D21E6" },
+    notify_close: { close_n: true, color: "#DDDEE2" },
+    notify_color: {
+      background: "#F7F7F9",
+      text: "#DAD9DE",
+      border: {
+        type: true,
+        color: "5px solid #A78DF0",
+      },
+    },
+    progress_bar: {
+      type: false,
+      background: "#ff0000",
+    },
+  };
+
+  const notificationSettings3 = {
+    notify_text: "You have no credits left! Upgrade to continue.",
+    notify_desc: [
+      {
+        desc: false,
+        text: "Incorrect code may impact your website's performance",
+      },
+    ],
+    notify_btn: { btn: false, text: "View the data" },
+    redirect_url: { redirect: true, url: "#", text: "Upgrade" },
+    notify_icon: { icon: true, color: "#F9BF54" },
+    notify_close: { close_n: false, color: "#DDDEE2" },
+    notify_color: {
+      background: "#FFF9EB",
+      text: "#775500",
+      border: {
+        type: false,
+        color: "5px solid #A78DF0",
+      },
+    },
+    progress_bar: {
+      type: true,
+      background: "#F9BF54",
+    },
+  };
+
   return (
-    <div>
-      <div>
-        <SmartNotification {...notificationSettings} />
-      </div>
+    <>
+      <SmartNotification {...notificationSettings} />
+      <SmartNotification {...notificationSettings2} />
+      <SmartNotification {...notificationSettings3} />
 
       {/* <button onClick={handleClick}>Click to Open</button> */}
-    </div>
+    </>
   );
 };
 
--- src/SmartNotification.jsx
+++ src/SmartNotification.jsx
@@ -5,18 +5,22 @@ const SmartNotification = ({
   notify_text,
   notify_desc,
   notify_btn,
-  button_text,
   redirect_url,
   notify_icon,
   notify_close,
   notify_color,
+  progress_bar,
 }) => {
   const [openNotify, setOpenNotify] = useState(true);
   const [timer, setTimer] = useState(null);
   const [progress, setProgress] = useState(0);
 
+  console.log(progress);
+  console.log("notify_color:", notify_icon);
+  console.log("notify_color.icon:", notify_icon?.icon);
+
   useEffect(() => {
-    if (openNotify) {
+    if (openNotify && progress_bar.type == true) {
       const interval = setInterval(() => {
         setProgress((prev) => {
           if (prev >= 100) {
@@ -39,10 +43,19 @@ const SmartNotification = ({
   return (
     // openNotify && (
     <div className="smart-notification">
-      <div className="notification info" style={{ position: "relative" }}>
+      <div
+        className="notification"
+        style={{
+          position: "relative",
+          backgroundColor: `${notify_color.background}`,
+          borderLeft: `${
+            notify_color.border.type && notify_color.border.color
+          }`,
+        }}
+      >
         <div className="notification-wrap">
           <div className="icon-wrap">
-            {notify_icon && (
+            {notify_icon.icon && (
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 height="14"
@@ -50,28 +63,34 @@ const SmartNotification = ({
                 viewBox="0 0 512 512"
               >
                 <path
-                  fill={notify_color.icon}
+                  fill={notify_icon.color}
                   d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zm0-384c13.3 0 24 10.7 24 24l0 112c0 13.3-10.7 24-24 24s-24-10.7-24-24l0-112c0-13.3 10.7-24 24-24zM224 352a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z"
                 />
               </svg>
             )}
-            <h4>{notify_text}</h4>
+            <h4 style={{ color: `${notify_color.text}` }}>{notify_text}</h4>
           </div>
-          {/* <p>{notify_desc}</p> */}
-          <ul>
-            {notify_desc.map((item, index) => (
-              <li key={index}>{item.text}</li>
-            ))}
-          </ul>
-          {notify_btn && (
+
+          {notify_desc.some((item) => item.desc) && (
+            <ul>
+              {notify_desc.map((item, index) =>
+                item.desc ? <li key={index}>{item.text}</li> : null
+              )}
+            </ul>
+          )}
+
+          {notify_btn.btn && (
             <div className="export-option">
-              <button>View the data</button>
-              <a href="#">Maybe later</a>
+              <button>{notify_btn.text}</button>
             </div>
           )}
         </div>
-        {redirect_url && <a href={redirect_url}>Upgrade</a>}
-        {notify_close && (
+        {redirect_url.redirect && (
+          <a href={redirect_url.url} style={{ color: `${notify_color.text}` }}>
+            {redirect_url.text}
+          </a>
+        )}
+        {notify_close.close_n && (
           <div
             style={{ marginLeft: "auto" }}
             onClick={() => setOpenNotify(false)}
@@ -81,25 +100,28 @@ const SmartNotification = ({
               height="14"
               width="10.5"
               viewBox="0 0 384 512"
+              cursor="pointer"
             >
               <path
-                fill="#cac9c9"
+                fill={notify_close.color}
                 d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"
               />
             </svg>
           </div>
         )}
-        <div
-          style={{
-            position: "absolute",
-            bottom: 0,
-            right: 0,
-            height: "5px",
-            backgroundColor: "#76c7c0",
-            width: `${progress}%`,
-            transition: "width 0.1s linear",
-          }}
-        ></div>
+        {progress_bar.type && (
+          <div
+            style={{
+              position: "absolute",
+              bottom: 0,
+              right: 0,
+              height: "5px",
+              backgroundColor: `${progress_bar.background}`,
+              width: `${progress}%`,
+              transition: "width 0.1s linear",
+            }}
+          ></div>
+        )}
       </div>
     </div>
     // )
